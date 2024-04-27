7a3ba55846ab262194e6b43b3110453145e842ac
Jozuan Lopez <jozuanlopez@gmail.com>
2024-04-27 16:28:18 -0600

    Ends up the midterm exercise

--- Midterm2Progra1/Program.cs
+++ Midterm2Progra1/Program.cs
@@ -2,6 +2,36 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        double weight, height;
+        double imc = 0;
+
+        Console.WriteLine("--------- CALCULADORA DE IMC ---------");
+        Console.WriteLine("Ingrese su peso (KG): ");
+        weight = double.Parse(Console.ReadLine() ?? "");
+
+        Console.WriteLine("Ingrese su altura: ");
+        height = double.Parse(Console.ReadLine() ?? "");
+
+        if (weight > 0 && height > 0)
+        {
+            imc = weight / (height * 2);
+        }
+
+        if (imc > 30)
+        {
+            Console.WriteLine("ALERTA: Tienes obesidad");
+        }
+        else if (imc > 25 && imc <= 30)
+        {
+            Console.WriteLine("Tienes sobrepeso");
+        }
+        else if (imc > 18.5 && imc <= 25)
+        {
+            Console.WriteLine("Felicidades, tienes un peso normal");
+        }
+        else if (imc < 18.5)
+        {
+            Console.WriteLine("ALERTA: tienes bajo peso");
+        }
     }
 }
