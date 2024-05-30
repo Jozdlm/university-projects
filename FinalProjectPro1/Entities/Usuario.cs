c6577f3318ea89c33f67f4576e5692ff7c4d112a
Jozuan Lopez <jozuanlopez@gmail.com>
2024-05-29 19:59:01 -0600

    Moves Usuario class to their own file

---
+++ FinalProjectPro1/Entities/Usuario.cs
@@ -0,0 +1,25 @@
+﻿namespace FinalProjectPro1;
+
+public class Usuario
+{
+    public required string Nombre { get; set; }
+    public required string Contraseña { get; set; }
+    public bool EsAdministrador { get; set; }
+    public List<Prestamo> Prestamos { get; set; } = new List<Prestamo>();
+
+    public string ToCsv()
+    {
+        return $"{Nombre},{Contraseña},{EsAdministrador}";
+    }
+
+    public static Usuario FromCsv(string csvLine)
+    {
+        var values = csvLine.Split(',');
+        return new Usuario
+        {
+            Nombre = values[0],
+            Contraseña = values[1],
+            EsAdministrador = bool.Parse(values[2])
+        };
+    }
+}
--- FinalProjectPro1/Program.cs
+++ FinalProjectPro1/Program.cs
@@ -1,26 +1,4 @@
-﻿public class Usuario
-{
-    public string Nombre { get; set; }
-    public string Contraseña { get; set; }
-    public bool EsAdministrador { get; set; }
-    public List<Prestamo> Prestamos { get; set; } = new List<Prestamo>();
-
-    public string ToCsv()
-    {
-        return $"{Nombre},{Contraseña},{EsAdministrador}";
-    }
-
-    public static Usuario FromCsv(string csvLine)
-    {
-        var values = csvLine.Split(',');
-        return new Usuario
-        {
-            Nombre = values[0],
-            Contraseña = values[1],
-            EsAdministrador = bool.Parse(values[2])
-        };
-    }
-}
+﻿using FinalProjectPro1;
 
 public class Libro
 {
