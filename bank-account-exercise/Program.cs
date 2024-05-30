47ed1a4b091956fbbadb347808cc777ea853b7f6
Jozuan Lopez <jozuanlopez@gmail.com>
2024-05-29 19:46:24 -0600

    Adds new completed excercise

---
+++ bank-account-exercise/Program.cs
@@ -0,0 +1,47 @@
+﻿class BankAccount
+{
+    private double Balance = 0;
+
+    public void IncreaseAsset(double ammount)
+    {
+        Balance = Balance + ammount;
+    }
+
+    public void DecreaseAsset(double ammount)
+    {
+        if (Balance > ammount)
+        {
+            Balance = Balance - ammount;
+        }
+        else
+        {
+            Console.WriteLine("Saldo insuficiente");
+        }
+    }
+
+    public void ShowBalance()
+    {
+        Console.WriteLine($"El balance de la cuenta es: {Balance}");
+    }
+}
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Bienvenido!");
+        BankAccount account = new BankAccount();
+        Console.Write($"Saldo inicial: ");
+        account.ShowBalance();
+
+        // Hacemos depositos
+        account.IncreaseAsset(150);
+        account.IncreaseAsset(450);
+
+        // Hacemos retiros
+        account.DecreaseAsset(37);
+
+        // Mostramos el saldo
+        account.ShowBalance();
+    }
+}
