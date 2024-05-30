8a4fd09b035e06639881c2e52cc7f6434fd821fa
Jozuan Lopez <jozuanlopez@gmail.com>
2024-05-29 20:00:25 -0600

    Moves libro class to their own file

---
+++ FinalProjectPro1/Entities/Libro.cs
@@ -0,0 +1,33 @@
+﻿namespace FinalProjectPro1;
+
+public class Libro
+{
+    public required string Nombre { get; set; }
+    public required string Autor { get; set; }
+    public int AñoPublicacion { get; set; }
+    public int AñoEdicion { get; set; }
+    public int NumeroEdicion { get; set; }
+    public required string Genero { get; set; }
+    public int CantidadInventario { get; set; }
+    public bool DisponibleParaPrestamo { get { return CantidadInventario > 0; } }
+
+    public string ToCsv()
+    {
+        return $"{Nombre},{Autor},{AñoPublicacion},{AñoEdicion},{NumeroEdicion},{Genero},{CantidadInventario}";
+    }
+
+    public static Libro FromCsv(string csvLine)
+    {
+        var values = csvLine.Split(',');
+        return new Libro
+        {
+            Nombre = values[0],
+            Autor = values[1],
+            AñoPublicacion = int.Parse(values[2]),
+            AñoEdicion = int.Parse(values[3]),
+            NumeroEdicion = int.Parse(values[4]),
+            Genero = values[5],
+            CantidadInventario = int.Parse(values[6])
+        };
+    }
+}
--- FinalProjectPro1/Program.cs
+++ FinalProjectPro1/Program.cs
@@ -1,37 +1,5 @@
 ﻿using FinalProjectPro1;
 
-public class Libro
-{
-    public string Nombre { get; set; }
-    public string Autor { get; set; }
-    public int AñoPublicacion { get; set; }
-    public int AñoEdicion { get; set; }
-    public int NumeroEdicion { get; set; }
-    public string Genero { get; set; }
-    public int CantidadInventario { get; set; }
-    public bool DisponibleParaPrestamo { get { return CantidadInventario > 0; } }
-
-    public string ToCsv()
-    {
-        return $"{Nombre},{Autor},{AñoPublicacion},{AñoEdicion},{NumeroEdicion},{Genero},{CantidadInventario}";
-    }
-
-    public static Libro FromCsv(string csvLine)
-    {
-        var values = csvLine.Split(',');
-        return new Libro
-        {
-            Nombre = values[0],
-            Autor = values[1],
-            AñoPublicacion = int.Parse(values[2]),
-            AñoEdicion = int.Parse(values[3]),
-            NumeroEdicion = int.Parse(values[4]),
-            Genero = values[5],
-            CantidadInventario = int.Parse(values[6])
-        };
-    }
-}
-
 public class Prestamo
 {
     public Libro Libro { get; set; }
