7ed7f73b530332cac8a7ae705a02af9100ed660a
Jozuan Lopez <jozuanlopez@gmail.com>
2024-05-29 20:02:29 -0600

    Moves prestamo class to their own file

---
+++ FinalProjectPro1/Entities/Prestamo.cs
@@ -0,0 +1,29 @@
+﻿namespace FinalProjectPro1;
+
+public class Prestamo
+{
+    public required Libro Libro { get; set; }
+    public required Usuario Usuario { get; set; }
+    public DateTime FechaPrestamo { get; set; }
+    public DateTime FechaDevolucion { get; set; }
+
+    public string ToCsv()
+    {
+        return $"{Libro.Nombre},{Usuario.Nombre},{FechaPrestamo},{FechaDevolucion}";
+    }
+
+    public static Prestamo FromCsv(string csvLine, List<Libro> libros, List<Usuario> usuarios)
+    {
+        var values = csvLine.Split(',');
+        var libro = libros.FirstOrDefault(l => l.Nombre == values[0]);
+        var usuario = usuarios.FirstOrDefault(u => u.Nombre == values[1]);
+        
+        return new Prestamo
+        {
+            Libro = libro,
+            Usuario = usuario,
+            FechaPrestamo = DateTime.Parse(values[2]),
+            FechaDevolucion = DateTime.Parse(values[3])
+        };
+    }
+}
--- FinalProjectPro1/Program.cs
+++ FinalProjectPro1/Program.cs
@@ -1,32 +1,5 @@
 ﻿using FinalProjectPro1;
 
-public class Prestamo
-{
-    public Libro Libro { get; set; }
-    public Usuario Usuario { get; set; }
-    public DateTime FechaPrestamo { get; set; }
-    public DateTime FechaDevolucion { get; set; }
-
-    public string ToCsv()
-    {
-        return $"{Libro.Nombre},{Usuario.Nombre},{FechaPrestamo},{FechaDevolucion}";
-    }
-
-    public static Prestamo FromCsv(string csvLine, List<Libro> libros, List<Usuario> usuarios)
-    {
-        var values = csvLine.Split(',');
-        var libro = libros.FirstOrDefault(l => l.Nombre == values[0]);
-        var usuario = usuarios.FirstOrDefault(u => u.Nombre == values[1]);
-        return new Prestamo
-        {
-            Libro = libro,
-            Usuario = usuario,
-            FechaPrestamo = DateTime.Parse(values[2]),
-            FechaDevolucion = DateTime.Parse(values[3])
-        };
-    }
-}
-
 public class SistemaBiblioteca
 {
     private List<Usuario> usuarios = new List<Usuario>();
