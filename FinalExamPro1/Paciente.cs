fd15cf1e38049a979b88ae19befeb3998fa48953
Jozuan Lopez <jozuanlopez@gmail.com>
2024-06-08 12:09:42 -0600

    Defines the classes Receta and Paciente

---
+++ FinalExamPro1/Paciente.cs
@@ -0,0 +1,24 @@
+﻿namespace FinalExamPro1;
+
+public class Paciente
+{
+    public required string Nombre { get; set; }
+    public required int Edad { get; set; }
+    public List<Receta> Recetas { get; set; } = new List<Receta>();
+
+    public void AgregarReceta(Receta receta)
+    {
+        Recetas.Add(receta);
+    }
+
+    public void VerListadoRecetas()
+    {
+        Console.WriteLine($"Recetas del paciente {Nombre}");
+        
+        foreach (var receta in Recetas)
+        {
+            Console.WriteLine($"Medicamento: {receta.Nombre}");
+            Console.WriteLine($"Dosis: {receta.Dosis}");
+        }
+    }
+}
---
+++ FinalExamPro1/Receta.cs
@@ -0,0 +1,8 @@
+﻿namespace FinalExamPro1;
+
+public class Receta
+{
+    public required string Nombre {get; set;}
+    public required int Dosis {get; set;}
+    public required string TipoDosis {get; set;}
+}
