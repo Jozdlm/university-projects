d0a162fc0fec7b464d3f55399d22af60faca418d
Jozuan Lopez <jozuanlopez@gmail.com>
2024-06-08 12:10:47 -0600

    Defines Medico class

---
+++ FinalExamPro1/Medico.cs
@@ -0,0 +1,11 @@
+﻿namespace FinalExamPro1;
+
+public class Medico
+{
+    public required string Nombre {get; set;}
+    public required int Edad {get; set;}
+
+    public void RecetarMedicamento(Receta receta, Paciente paciente) {
+        paciente.AgregarReceta(receta);
+    }
+}
