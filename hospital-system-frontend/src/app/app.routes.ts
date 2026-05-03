022e73fe19c595dc564711e41e807f8f87f89ecf
Jozuan Martínez <jozuanlopez@gmail.com>
2026-05-02 19:39:27 -0600

    Add route guards for authentication and role-based access control

--- hospital-system-frontend/src/app/app.routes.ts
+++ hospital-system-frontend/src/app/app.routes.ts
@@ -2,17 +2,24 @@ import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
 import { Reports } from './pages/reports/reports';
 import { Tickets } from './pages/tickets/tickets';
+import { adminGuard } from './modules/auth/admin-guard';
+import { frontDeskGuard } from './modules/auth/front-desk-guard';
+import { authGuard } from './modules/auth/auth-guard';
+import { guestGuard } from './modules/auth/guest-guard';
 
 export const routes: Routes = [
   {
     path: 'auth/login',
     component: Login,
+    canActivate: [guestGuard],
   },
   {
     path: 'app',
+    canActivate: [authGuard],
+    canActivateChild: [authGuard],
     children: [
-      { path: 'tickets', component: Tickets, pathMatch: 'full' },
-      { path: 'reports', component: Reports },
+      { path: 'tickets', component: Tickets, canActivate: [frontDeskGuard] },
+      { path: 'reports', component: Reports, canActivate: [adminGuard] },
     ],
   },
 ];
