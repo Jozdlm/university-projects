2eced32907fe3c1a490027177424dc520ce0f7f8
Jozuan Martínez <jozuanlopez@gmail.com>
2026-05-09 23:50:22 -0600

    Add API_URL injection token and update app configuration to include API endpoint

--- hospital-system-frontend/src/app/app.config.ts
+++ hospital-system-frontend/src/app/app.config.ts
@@ -1,9 +1,14 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import {
+  ApplicationConfig,
+  InjectionToken,
+  provideBrowserGlobalErrorListeners,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
 import { provideHttpClient } from '@angular/common/http';
+import { API_URL } from './di-tokens';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -11,5 +16,6 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideCharts(withDefaultRegisterables()),
     provideHttpClient(),
+    { provide: API_URL, useValue: 'https://hospital-university-production.up.railway.app/api' },
   ],
 };
